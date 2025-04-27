d10cb8097ed887764c4ea8cfbe7e0d7a3dc215ac
Eugene233 <agboreugene@gmail.com>
2025-04-28 00:27:11 +0200

    refactor code

--- linear_regression.py
+++ linear_regression.py
@@ -6,25 +6,18 @@ class MyLinearRegression:
     def __init__(self, iters=1000):
         self.iters = iters
         self.intercept = None
-        self.last_beta = None
+        self.beta_hat = None
 
 
     def fit(self, X, y):
 
         # We are using the closed-form method to obtain the
-        # which permits us to get beta immediately
-        # get different values of betas after many iterations
+        # optimal beta_hat we will use for prediction
 
-        betas = []
-        for i in range(self.iters):
-            XTX = np.dot(X.T, X)
-            xTy = np.dot(X.T, y)
+        XTX = np.dot(X.T, X)
+        xTy = np.dot(X.T, y)
 
-            beta_hat = np.dot(np.linalg.inv(XTX), xTy)
-
-            betas.append(beta_hat)
-
-        self.last_beta = betas[-1]
+        self.beta_hat = np.dot(np.linalg.inv(XTX), xTy)
 
 
     def predict(self, X_test):
@@ -34,7 +27,7 @@ class MyLinearRegression:
         returns: y_new_pred - A model prediction 
         """
 
-        y_new_pred = np.dot(X_test, self.last_beta)
+        y_new_pred = np.dot(X_test, self.beta_hat)
         return y_new_pred
 
 
